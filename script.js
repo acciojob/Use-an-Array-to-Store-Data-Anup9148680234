02e73e1836558a3a0aae2cefee37ae5bbb5b4379
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-03-18 13:31:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+let yourArray = [
+	[
+    {
+      one: 1,
+      two: 2
+    }
 
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    }
+  ],
+  [
+    {
+      a: true,
+      b: false
+    }
+  ]
+	,
+  [
+    {
+      a: true,
+      b: false
+    }
+  ]
+	,
+  [
+    {
+      a: true,
+      b: false
+    }
+  ]
+]
